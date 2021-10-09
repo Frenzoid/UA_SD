22a6529383c3283d16f17f52cb0c43fd36b6417e
Frenzoid <frenzoid@protonmail.com>
2021-10-09 14:09:51 +0200

    rendered map

--- Practices/practica 2/fwq_visitor/src/components/map.js
+++ Practices/practica 2/fwq_visitor/src/components/map.js
@@ -6,6 +6,8 @@ function Map(props) {
     const socket = props.socket;
 
     const [user, setUser] = [props.user, props.setUser];
+    const [matrix, setMatrix] = useState([]);
+    let atracciones;
 
     useEffect(() => {
         // Si el usuario no esta registrado, no le permitimos acceder a esta página.
@@ -13,7 +15,60 @@ function Map(props) {
             history.push("/");
 
         bindSokets();
+
+        const n = 20;
+        for (let i = 0; i < n; i++) {
+            // Columnas (array vacio).
+            matrix[i] = [];
+            for (let j = 0; j < n; j++) {
+                // Filas ( populadas )
+                matrix[i][j] = { color: "blue" }
+            }
+        }
+
+        atracciones = [{ coords: [2, 2] }, { coords: [2, 17] }, { coords: [17, 2] }, { coords: [17, 17] }]
+        atracciones.forEach((attr) => {
+            matrix[attr.coords[0]][attr.coords[1]].color = "purple";
+        });
+
+        setMatrix([...matrix]);
+
+
+        setInterval(() => {
+            console.log(user);
+
+            if (user.x_actual == user.x_destino && user.y_actual == user.y_destino) {
+                seleccionaAtraccion();
+            }
+
+        }, 1000)
+
+
     }, []);
+
+    let siguientePosicion = () => {
+
+        redibujaCasilla(user.x_actual, user.y_actual);
+    }
+
+    let randomIntFromInterval = (min, max) => {
+        // min and max included
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    let seleccionaAtraccion = () => {
+        const numAtracciones = atracciones.length;
+        const attrNum = randomIntFromInterval(0, numAtracciones - 1);
+        user.x_destino = atracciones[attrNum].coords[0] + 1;
+        user.y_destino = atracciones[attrNum].coords[1] + 1;
+    }
+
+    let redibujaCasilla = (i1, j1, i2, j2) => {
+        matrix[i1 - 1][j1 - 1].color = "blue";
+        matrix[i2 - 1][j2 - 1].color = "red";
+
+        setMatrix([...matrix]);
+    }
 
     let bindSokets = () => {
         socket.on("usuario_desregistrado", () => { setUser({}); history.push("/"); })
@@ -35,6 +90,29 @@ function Map(props) {
             <code>
                 {JSON.stringify(user)}
             </code>
+
+            {matrix.map((i, ipos) => {
+
+                return (
+                    <div key={ipos} className="d-flex flex-row no-wrap justify-content-center">
+
+                        {i.map((j, jpos) => {
+
+                            return (
+                                <div
+                                    style={{ minWidth: "4%", minHeight: "30px", textAlign: "center", color: "white", backgroundColor: matrix[ipos][jpos].color }}
+                                    className="card m-1"
+                                    key={ipos + ", " + jpos}
+                                >
+                                    {ipos + ", " + jpos}
+                                </div>
+                            )
+                        })}
+
+                    </div>
+                )
+
+            })}
         </div>
     )
 
