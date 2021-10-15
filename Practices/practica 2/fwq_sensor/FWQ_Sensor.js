e1ddd8ba8d4d28183c8362bf780c15366c00e54a
Frenzoid <frenzoid@protonmail.com>
2021-10-15 13:10:00 +0200

    finished sensores

--- Practices/practica 2/fwq_sensor/FWQ_Sensor.js
+++ Practices/practica 2/fwq_sensor/FWQ_Sensor.js
@@ -4,10 +4,12 @@ let client = new kafka.KafkaClient({ kafkaHost: 'oldbox.cloud:9092' });
 
 let producer = new kafka.Producer(client);
 
-let payloads = [{ topic: 'numeroPersonas', messages: JSON.stringify({ /*Habría que poner */}), partition: 0 }];
+let payloads = [{ topic: 'numeroPersonas', messages: "", partition: 0 }];
 
-const kafkaWebSocket = props.kafkaWebSocket;
-const [atraccion, setAtraccion] = [props.atraccion, props.setAtraccion];
+let idAttr = process.env.IDATTR || process.argv[2];
+
+if (!idAttr) throw Exception("Falta la ID de la atraccion");
+
 
 let randomIntFromInterval = (min, max) => {
     // min and max included
@@ -17,11 +19,12 @@ let randomIntFromInterval = (min, max) => {
 producer.on('ready', () => {
 
   setInterval(() => {
-    kafkaWebSocket.emit("atracción", atraccion);
-    payloads[0].messages = JSON.stringify({ randomIntFromInterval( a = 0,  b = 100); });
+    payloads[0].messages = JSON.stringify({ personas: randomIntFromInterval(0, 100), idAttr });
+
     producer.send(payloads, (err, data) => {
       console.log(data);
     });
+
   }, 2000);
 
 });
