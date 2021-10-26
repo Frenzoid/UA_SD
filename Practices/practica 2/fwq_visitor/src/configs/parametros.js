b00c04c6bfa25813dc6048865241430bb9e4eb8b
Frenzoid <frenzoid@protonmail.com>
2021-10-26 08:59:52 +0200

    visitor dockerfile finished, passed envs to .env

--- Practices/practica 2/fwq_visitor/src/configs/parametros.js
+++ Practices/practica 2/fwq_visitor/src/configs/parametros.js
@@ -1,7 +1,7 @@
-const REGISTRYADDRESS = process.env.REGISTRYADDRESS || "http://localhost:9090";
-const KAFKACONTROLLER = process.env.KAFKACONTROLLER || "http://localhost:9111";
-const KAFKAADDRESS = process.env.KAFKAADDRESS || "oldbox.cloud:9092";
-const SECRET = process.env.SECRET || "ABRACADABRA";
+const REGISTRYADDRESS = process.env.REACT_APP_REGISTRYADDRESS;
+const KAFKACONTROLLER = process.env.REACT_APP_KAFKACONTROLLER;
+const KAFKAADDRESS = process.env.REACT_APP_KAFKAADDRESS;
+const SECRET = process.env.REACT_APP_SECRET;
 
 console.log({ config: { REGISTRYADDRESS, KAFKAADDRESS, KAFKACONTROLLER, SECRET }});
 
