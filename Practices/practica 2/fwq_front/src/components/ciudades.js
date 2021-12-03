d51f8e5d0f2073c842a27756f86cc74387f5dbf9
Frenzoid <frenzoid@protonmail.com>
2021-12-03 22:27:35 +0100

    made all axios request error responsive

--- Practices/practica 2/fwq_front/src/components/ciudades.js
+++ Practices/practica 2/fwq_front/src/components/ciudades.js
@@ -9,7 +9,10 @@ function Ciudades() {
 
     useEffect(() => {
         const inter = setInterval(async () => {
-            setCiudades((await Axios.get(APIENGINE + '/ciudades')).data);
+            await Axios.get(APIENGINE + '/ciudades').then((data) => {
+                setErrorMsg("");
+                setCiudades(data.data);
+            }).catch(err => setErrorMsg(err.message));
         }, REQUESTTIME);
 
         // Parar el bucle cuando se desrenderice el componente ( cuando se cambia a otra página )
@@ -25,61 +28,68 @@ function Ciudades() {
             setErrorMsg("");
         }).catch((err) => {
             console.log(err);
-            setErrorMsg(err.response.data.message);
+            setErrorMsg(err.response.data.message || err.error);
         });
     }
 
     return (
         <div className="container">
+            {errorMsg ?
+                <div style={{ width: "100%" }} className="card card-header bg-danger text-white text-center mt-3">
+                    {errorMsg}
+                    <div className="text-center mt-2">
+                        <div className="spinner-border">
+                            <span className="sr-only"></span>
+                        </div>
+                    </div>
+                </div> : ""
+            }
             {ciudades == null ?
                 <div className="text-center">
                     <img src={"https://i.imgur.com/6Hq8096.gif"} className="mx-auto" />
                 </div>
-                :
-                <table className="table table-hover" >
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Temperatura</th>
-                            <th scope="col">Ultima Edicion</th>
-                            <th scope="col">Fecha Creacion</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {ciudades.map((log, index) => {
-                            return (
-                                <tr key={index}>
-                                    <th scope="row">{log.id}</th>
-                                    <td>{log.nombre}</td>
-                                    <td>{log.temperatura} Cº</td>
-                                    <td>{log.updatedAt}</td>
-                                    <td>{log.createdAt}</td>
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </table>
-            }
-            <form>
-                <h2 className="text-center">Editar Ciudad</h2>
-                <div className="row">
-                    <div className="col">
-                        <input type="text" onChange={(e) => { setCiudad({ ...ciudad, id: e.target.value }) }} className="form-control" placeholder="id"></input>
-                    </div>
-                    <div className="col">
-                        <input type="text" onChange={(e) => { setCiudad({ ...ciudad, nombre: e.target.value }) }} className="form-control" placeholder="Nombre ciudad"></input>
-                    </div>
-                </div>
-                <div className="text-center mt-2">
-                    <button onClick={(e) => { e.preventDefault(); cambiarCiudad(); }} className="btn btn-primary">Actualizar</button>
-                </div>
-            </form>
-            {!errorMsg ? "" :
-                <div style={{ width: "100%" }} className="card card-header bg-danger text-white text-center mt-3">
-                    {errorMsg}
+                : <div>
+                    <table className="table table-hover" >
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Temperatura</th>
+                                <th scope="col">Ultima Edicion</th>
+                                <th scope="col">Fecha Creacion</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {ciudades.map((log, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <th scope="row">{log.id}</th>
+                                        <td>{log.nombre}</td>
+                                        <td>{log.temperatura} Cº</td>
+                                        <td>{log.updatedAt}</td>
+                                        <td>{log.createdAt}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                    <form>
+                        <h2 className="text-center">Editar Ciudad</h2>
+                        <div className="row">
+                            <div className="col">
+                                <input type="text" onChange={(e) => { setCiudad({ ...ciudad, id: e.target.value }) }} className="form-control" placeholder="id"></input>
+                            </div>
+                            <div className="col">
+                                <input type="text" onChange={(e) => { setCiudad({ ...ciudad, nombre: e.target.value }) }} className="form-control" placeholder="Nombre ciudad"></input>
+                            </div>
+                        </div>
+                        <div className="text-center mt-2">
+                            <button onClick={(e) => { e.preventDefault(); cambiarCiudad(); }} className="btn btn-primary">Actualizar</button>
+                        </div>
+                    </form>
                 </div>
             }
+
         </div>
     )
 }
