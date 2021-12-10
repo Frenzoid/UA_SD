4e3c7691552456a9929e30ce1dea1026a28bf450
Frenzoid <frenzoid@protonmail.com>
2021-12-10 14:36:54 +0100

    made visitor accept ssl unauthorized

--- Practices/practica 2/fwq_registry/servers.js
+++ Practices/practica 2/fwq_registry/servers.js
@@ -25,7 +25,7 @@ const http = require('http')
     cert: fs.readFileSync('certs/server.cert')
 }, 
 */
-const server = http.createServer(app);
+const server = http.createServer({}, app);
 
 const io = require("socket.io")(server, {
     cors: { origin: "*", }
